a0667d95232ffd7c378f31f72556eba70bee5cb9
Florian Slob <florian.slob@teamrockstars.nl>
2022-02-01 18:40:40 +0100

    Created service and UT Class And check for correct result for the number 1

---
+++ FizzBuzz/FizzBuzz.Core/FizzBuzzService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace FizzBuzz.Core
+{
+    public class FizzBuzzService
+    {
+        public IEnumerable<char> GetFizzBuzzForNumber(int i)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,24 @@
+﻿using FizzBuzz.Core;
+using Xunit;
+using Xunit.Sdk;
+
+namespace FizzBuzz.UnitTests
+{
+    public class FizzBuzzTests : TestClass
+    {
+        [Fact]
+        public void ServiceReturnsNumberOne()
+        {
+            // ARRANGE
+            var service = new FizzBuzzService();
+            
+            // ACT
+            var result = service.GetFizzBuzzForNumber(1);
+
+            // ASSERT
+            Assert.Equal("1",result);
+
+        } 
+        
+    }
+}
