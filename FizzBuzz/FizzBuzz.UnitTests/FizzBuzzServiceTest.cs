ec924420ba3f4698dccf922bc61ffcb638f28309
FrankvanZ <fwj.vanzutphen@live.nl>
2022-02-01 18:45:59 +0100

    First TDD test

---
+++ FizzBuzz/FizzBuzz.UnitTests/FizzBuzzServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz.UnitTests
+{
+    [TestFixture]
+    internal class FizzBuzzServiceTest
+    {
+        [Test]
+        public void ThreeShouldReturnFizz()
+        {
+            // Arrange
+            var service = new FizzBuzzService();
+
+            // Act
+            var result = service.FizzBuzz(3);
+
+            // Assert
+            result.Should().NotBeNull().And.Be("Fizz");
+        }
+    }
+}
