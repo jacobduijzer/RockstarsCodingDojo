2f27ef578d284ee4891f7a9a9ff5707de6d02858
jwijnakker <jwijnakker@dearnova.nl>
2022-02-01 20:27:17 +0100

    refactor and all tests pass

--- GildedRose/GildedRose.UnitTests/UnitTest1.cs
+++ GildedRose/GildedRose.UnitTests/UnitTest1.cs
@@ -7,24 +7,25 @@ namespace GildedRose.UnitTests
     public class UnitTest1
     {
         [Theory]
-        [InlineData("foo", 1, 1, 0, 0)]
-        [InlineData("foo", 0, 10, -1, 8)]
-        [InlineData("foo", -1, 8, -2, 6)]
-        [InlineData("foo", 2, 0, 1, 0)]
-        [InlineData("foo", 0, 0, -1, 0)]
-        [InlineData("Aged Brie", 1, 0, 0, 1)]
-        [InlineData("Aged Brie", 0, 0, -1, 2)]
-        [InlineData("Aged Brie", 0, 49, -1, 50)]
-        [InlineData("Sulfuras, Hand of Ragnaros", 0, 10, 0, 10)]
-        [InlineData("Backstage passes to a TAFKAL80ETC concert", 11, 10, 10, 11)]	
-        [InlineData("Backstage passes to a TAFKAL80ETC concert", 6, 10, 5, 12)]	
-        [InlineData("Backstage passes to a TAFKAL80ETC concert", 5, 10, 4, 13)]	
-        [InlineData("Backstage passes to a TAFKAL80ETC concert", 0, 10, -1, 0)]	
+        [InlineData("foo", 1, 1, 0)]
+        [InlineData("foo", 0, 10, 8)]
+        [InlineData("foo", -1, 8, 6)]
+        [InlineData("foo", 2, 0, 0)]
+        [InlineData("foo", 0, 0, 0)]
+        [InlineData("Aged Brie", 1, 0, 1)]
+        [InlineData("Aged Brie", 0, 0, 2)]
+        [InlineData("Aged Brie", 0, 49, 50)]
+        [InlineData("Sulfuras, Hand of Ragnaros", 0, 10, 10, 0)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 11, 10, 11)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 6, 10, 12)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 5, 10, 13)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 1, 10, 13)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 0, 10, 0)]
         public void UpdateQuality_AfterSellInTwice(string itemName,
                                                    int sellIn,
                                                    int quality,
-                                                   int expectedSellIn,
-                                                   int expectedQuality)
+                                                   int expectedQuality,
+                                                   int expectedSellInOffset = -1)
         {
             // ARRANGE
             IList<Item> items = new List<Item> { new Item { Name = itemName,
@@ -36,7 +37,7 @@ namespace GildedRose.UnitTests
             app.UpdateQuality();
 
             // ASSERT
-            Assert.Equal(expectedSellIn, items[0].SellIn);
+            Assert.Equal(sellIn + expectedSellInOffset, items[0].SellIn);
             Assert.Equal(expectedQuality, items[0].Quality);
         }
 
