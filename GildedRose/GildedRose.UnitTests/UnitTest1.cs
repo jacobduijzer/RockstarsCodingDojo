097f50d072ef5d817a5de203d04d62faeda93138
jwijnakker <jwijnakker@dearnova.nl>
2022-02-01 20:13:04 +0100

    add testcases

--- GildedRose/GildedRose.UnitTests/UnitTest1.cs
+++ GildedRose/GildedRose.UnitTests/UnitTest1.cs
@@ -7,17 +7,22 @@ namespace GildedRose.UnitTests
     public class UnitTest1
     {
         [Theory]
-        [InlineData(1, 1, 0, 0)]
-        [InlineData(0, 10, -1, 8)]
-        [InlineData(-1, 8, -2, 6)]
-        [InlineData(0, -1, 0, 0)]
-        public void UpdateQuality_AfterSellInTwice(int sellIn,
+        [InlineData("foo", 1, 1, 0, 0)]
+        [InlineData("foo", 0, 10, -1, 8)]
+        [InlineData("foo", -1, 8, -2, 6)]
+        [InlineData("foo", 2, 0, 1, 0)]
+        [InlineData("foo", 0, 0, -1, 0)]
+        [InlineData("Aged Brie", 1, 0, 0, 1)]
+        [InlineData("Aged Brie", 0, 0, -1, 2)]
+        [InlineData("Aged Brie", 0, 49, -1, 50)]
+        public void UpdateQuality_AfterSellInTwice(string itemName,
+                                                   int sellIn,
                                                    int quality,
                                                    int expectedSellIn,
                                                    int expectedQuality)
         {
             // ARRANGE
-            IList<Item> items = new List<Item> { new Item { Name = "foo",
+            IList<Item> items = new List<Item> { new Item { Name = itemName,
                     SellIn = sellIn,
                     Quality = quality } };
             ConsoleApp.GildedRose app = new ConsoleApp.GildedRose(items);
@@ -29,5 +34,7 @@ namespace GildedRose.UnitTests
             Assert.Equal(expectedSellIn, items[0].SellIn);
             Assert.Equal(expectedQuality, items[0].Quality);
         }
+
+
     }
 }
