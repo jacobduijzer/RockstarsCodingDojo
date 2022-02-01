dbfe7460e6edb08a368288d558f125990ba0fb62
Bart van Nierop <mail@bvnierop.nl>
2022-02-01 19:04:46 +0100

    Add new test

--- FizzBuzz/FizzBuzz.UnitTests/FizzBuzzServiceTest.cs
+++ FizzBuzz/FizzBuzz.UnitTests/FizzBuzzServiceTest.cs
@@ -12,7 +12,7 @@ namespace FizzBuzz.UnitTests
     {
         [TestCase(3, "Fizz")]
         [TestCase(5, "Buzz")]
-        [TestCase(15, "FizzBuzz")]
+        [TestCase(15, "FizzBuzz")] // Divisible by 3 _and_ 5
         public void FizzBuzzTest(int input, string output)
         {
             // Arrange
@@ -23,6 +23,19 @@ namespace FizzBuzz.UnitTests
 
             // Assert
             result.Should().NotBeNull().And.Be(output);
+        }
+
+        [Test]
+        public void  FizzBuzz_ReturnsTheNumber_ForOtherNumbers()
+        {
+            // Arrange
+            var service = new FizzBuzzService(); // Duplicatation
+
+            // Act
+            var result = service.FizzBuzz(7);
+
+            // Assert
+            result.Should().Be("7");
         }
     }
 }
