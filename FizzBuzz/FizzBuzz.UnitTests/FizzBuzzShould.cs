845e6dec541f37c89f1350ec332844e8e5c0d5cb
Yorrick Spierings <yi.spierings@gmail.com>
2022-02-01 18:49:24 +0100

    bagger

---
+++ FizzBuzz/FizzBuzz.UnitTests/FizzBuzzShould.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace FizzBuzz.UnitTests
+{
+
+    public class FizzBuzzShould
+    {
+        [Theory]
+        [InlineData(1, "1")]
+        public void ReturnsNumberAsString(int number, string expected)
+        {
+            var unitUnderTest = new FizzBuzzGame();
+
+            string result = unitUnderTest.GetFizzBuzzResultForInputNumber(number);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
