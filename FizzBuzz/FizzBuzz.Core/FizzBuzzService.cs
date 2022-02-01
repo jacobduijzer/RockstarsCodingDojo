31891d8181914abce6e67ceea56a70cc306bdcab
Bart van Nierop <mail@bvnierop.nl>
2022-02-01 19:01:35 +0100

    Implement 15-case test

--- FizzBuzz/FizzBuzz.Core/FizzBuzzService.cs
+++ FizzBuzz/FizzBuzz.Core/FizzBuzzService.cs
@@ -4,6 +4,7 @@ namespace FizzBuzz.Core
     {
         public string FizzBuzz(int n)
         {
+            if (DivisibleBy(n, 15)) return "FizzBuzz";
             if (DivisibleBy(n, 3)) return "Fizz";
             if (DivisibleBy(n, 5)) return "Buzz";
             return "";
