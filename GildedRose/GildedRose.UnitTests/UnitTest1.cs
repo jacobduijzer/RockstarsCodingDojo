d6493ab6fe9f38d3e6f1519e5611878bfc37a351
Robert van Drunen <r.j.m.vandrunen@gmail.com>
2022-02-01 20:29:20 +0100

    100% coverage

--- GildedRose/GildedRose.UnitTests/UnitTest1.cs
+++ GildedRose/GildedRose.UnitTests/UnitTest1.cs
@@ -7,20 +7,6 @@ namespace GildedRose.UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Foo()
-        {
-            // ARRANGE
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            ConsoleApp.GildedRose app = new ConsoleApp.GildedRose(Items);
-
-            // ACT
-            app.UpdateQuality();
-
-            // ASSERT
-            Assert.Equal("fixme", Items[0].Name);
-        }
-
-        [Fact]
         public void ItemQualityShouldDegradeBy1()
         {
             // ARRANGE
@@ -65,19 +51,20 @@ namespace GildedRose.UnitTests
             Assert.Equal(9, Items[0].SellIn);
         }
 
-        [Fact]
-        public void AgedBrieQualityIncreasesTheOlderItGets()
+        [Theory]
+        [InlineData(10, 0, 1)]
+        [InlineData(0, 10, 11)]
+        public void AgedBrieQualityIncreasesTheOlderItGets(int sellIn, int initialQuality, int expectedQuality)
         {
             // ARRANGE
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = sellIn, Quality = initialQuality } };
             ConsoleApp.GildedRose app = new ConsoleApp.GildedRose(Items);
 
             // ACT
             app.UpdateQuality();
 
             // ASSERT
-            Assert.Equal(1, Items[0].Quality);
-            Assert.Equal(9, Items[0].SellIn);
+            Assert.Equal(expectedQuality, Items[0].Quality);
         }
 
         [Theory]       
@@ -94,6 +81,42 @@ namespace GildedRose.UnitTests
             // ASSERT
             Assert.Equal(expected, Items[0].Quality);
             Assert.Equal(9, Items[0].SellIn);
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(0)]
+        public void SulfurasNeverIncreasesOrDecreasesInQuality(int sellIn)
+        {
+            // ARRANGE
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = sellIn, Quality = 10 } };
+            ConsoleApp.GildedRose app = new ConsoleApp.GildedRose(Items);
+
+            // ACT
+            app.UpdateQuality();
+
+            // ASSERT
+            Assert.Equal(10, Items[0].Quality);
+        }
+        
+        
+
+        [Theory]
+        [InlineData(30, 10, 11)]
+        [InlineData(10, 10, 12)]
+        [InlineData(5, 10, 13)]
+        [InlineData(0, 10, 0)]
+        public void BackstagePassesChangeQualityDependingOnSellIn(int sellIn, int initialQuality, int expectedQuality)
+        {
+            // ARRANGE
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = initialQuality } };
+            ConsoleApp.GildedRose app = new ConsoleApp.GildedRose(Items);
+
+            // ACT
+            app.UpdateQuality();
+
+            // ASSERT
+            Assert.Equal(expectedQuality, Items[0].Quality);
         }
 
     }
