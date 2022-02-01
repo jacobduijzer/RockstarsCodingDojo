18d7f8813c61e323a9c6106f5fe55b8387a77619
Ilyas Gunduz <ilyas.gunduz@teamrockstars.nl>
2022-02-01 20:17:28 +0100

    ItemQualityNotMoreThan50 test written

--- GildedRose/GildedRose.UnitTests/UnitTest1.cs
+++ GildedRose/GildedRose.UnitTests/UnitTest1.cs
@@ -65,5 +65,36 @@ namespace GildedRose.UnitTests
             Assert.Equal(9, Items[0].SellIn);
         }
 
+        [Fact]
+        public void AgedBrieQualityIncreasesTheOlderItGets()
+        {
+            // ARRANGE
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 0 } };
+            ConsoleApp.GildedRose app = new ConsoleApp.GildedRose(Items);
+
+            // ACT
+            app.UpdateQuality();
+
+            // ASSERT
+            Assert.Equal(1, Items[0].Quality);
+            Assert.Equal(9, Items[0].SellIn);
+        }
+
+        [Theory]       
+        [InlineData("Aged Brie", 50, 50)]       
+        public void ItemQualityNotMoreThan50(string name, int quality, int expected)
+        {
+            // ARRANGE
+            IList<Item> Items = new List<Item> { new Item { Name = name, SellIn = 10, Quality = quality } };
+            ConsoleApp.GildedRose app = new ConsoleApp.GildedRose(Items);
+
+            // ACT
+            app.UpdateQuality();
+
+            // ASSERT
+            Assert.Equal(expected, Items[0].Quality);
+            Assert.Equal(9, Items[0].SellIn);
+        }
+
     }
 }
