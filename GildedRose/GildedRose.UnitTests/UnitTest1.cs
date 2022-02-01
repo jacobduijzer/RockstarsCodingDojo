ebba383251a67786ed2cf01de8eb034f48a3fe61
Florian Slob <florian.slob@teamrockstars.nl>
2022-02-01 20:13:08 +0100

    Added checks

--- GildedRose/GildedRose.UnitTests/UnitTest1.cs
+++ GildedRose/GildedRose.UnitTests/UnitTest1.cs
@@ -11,10 +11,10 @@ namespace GildedRose.UnitTests
         {
             var originalQuality = 10;
             var itemUnderTest = new Item {Name = "foo", SellIn = 1, Quality = originalQuality};
-            
+
             // ARRANGE
-            IList<Item> Items = new List<Item> {  itemUnderTest };
-            
+            IList<Item> Items = new List<Item> {itemUnderTest};
+
             ConsoleApp.GildedRose app = new ConsoleApp.GildedRose(Items);
 
             // ACT
@@ -22,55 +22,76 @@ namespace GildedRose.UnitTests
 
             var qualityDecreasedBeforeSellByDate = originalQuality - itemUnderTest.Quality;
             var qualityAfterFirstRun = itemUnderTest.Quality;
-            
+
             app.UpdateQuality();
             var qualityDecreasedAfterSellByDate = qualityAfterFirstRun - itemUnderTest.Quality;
 
             // ASSERT
             Assert.Equal(qualityDecreasedBeforeSellByDate * 2, qualityDecreasedAfterSellByDate);
         }
-        
+
         [Fact]
         public void QualityIsNeverNegative()
         {
-            var originalQuality = 10;
+            var originalQuality = 2;
             var itemUnderTest = new Item {Name = "foo", SellIn = 1, Quality = originalQuality};
-            
+
             // ARRANGE
-            IList<Item> Items = new List<Item> {  itemUnderTest };
-            
+            IList<Item> Items = new List<Item> {itemUnderTest};
+
             ConsoleApp.GildedRose app = new ConsoleApp.GildedRose(Items);
 
             // ACT
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 5; i++)
             {
                 app.UpdateQuality();
-                
-                // ASSERT
-                Assert.True(itemUnderTest.Quality >= 0);
             }
+            
+            // ASSERT
+            Assert.True(itemUnderTest.Quality >= 0);
         }
-        
+
         [Fact]
         public void AgedBrieGetsBetterQualityOverTime()
         {
             var originalQuality = 10;
             var itemUnderTest = new Item {Name = "Aged Brie", SellIn = 10, Quality = originalQuality};
-            
+
             // ARRANGE
-            IList<Item> Items = new List<Item> {  itemUnderTest };
-            
+            IList<Item> Items = new List<Item> {itemUnderTest};
+
             ConsoleApp.GildedRose app = new ConsoleApp.GildedRose(Items);
 
             // ACT
-            int previous
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 5; i++)
             {
                 app.UpdateQuality();
-                
-                // ASSERT
-                Assert.True(itemUnderTest.Quality >= 0);
             }
+
+            // ASSERT
+            Assert.True(itemUnderTest.Quality > originalQuality);
+            Assert.Equal(5, itemUnderTest.SellIn);
+        }
+        
+        [Fact]
+        public void NoItemHasQualityHigherThan50()
+        {
+            var originalQuality = 49;
+            var itemUnderTest = new Item {Name = "Aged Brie", SellIn = 10, Quality = originalQuality};
+
+            // ARRANGE
+            IList<Item> Items = new List<Item> {itemUnderTest};
+
+            ConsoleApp.GildedRose app = new ConsoleApp.GildedRose(Items);
+
+            // ACT
+            for (int i = 0; i < 5; i++)
+            {
+                app.UpdateQuality();
+            }
+
+            // ASSERT
+            Assert.True(itemUnderTest.Quality <= 50);
         }
     }
 }
