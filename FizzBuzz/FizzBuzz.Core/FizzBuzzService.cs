6600a10e0a33daf4c32616f884685f53ac7e6287
nils <nils.vaneijk@outlook.com>
2022-02-01 18:53:54 +0100

    Fix unit test

---
+++ FizzBuzz/FizzBuzz.Core/FizzBuzzService.cs
@@ -0,0 +1,11 @@
+// ReSharper disable once CheckNamespace
+namespace FizzBuzz
+{
+    public class FizzBuzzService
+    {
+        public string[] PrintNumbers()
+        {
+            return new []{""};
+        }
+    }
+}
